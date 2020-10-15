9873379988c4ba94e28e0d57ab75c3e45e4c77ea
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-10-14 23:17:19 -0400

    use theme colors in hamburger, tweak scale

--- frontend/src/components/Layout/Header/Hamburger.js
+++ frontend/src/components/Layout/Header/Hamburger.js
@@ -30,7 +30,8 @@ const Container = styled.div`
 const HamburgerIcon = styled.div`
   width: 3rem;
   height: 0.4rem;
-  background: ${props => (props.visible ? "rgba(0, 0, 0, 0)" : "#000")};
+  background: ${props =>
+    props.visible ? "rgba(0, 0, 0, 0)" : props.theme.colors.scale_3};
   transition: 0.5s ease-out all;
   border-radius: 2px;
   margin-top: 1rem;
@@ -41,19 +42,20 @@ const HamburgerIcon = styled.div`
     width: inherit;
     height: inherit;
     border-radius: inherit;
-    background: ${props => (props.visible ? "#fff" : "#000")};
+    background: ${props =>
+      props.visible ? props.theme.colors.scale_0 : props.theme.colors.scale_3};
     transition: 0.5s ease-out all;
   }
   &::before {
     transform: ${props =>
       props.visible
         ? "rotate(45deg) translate3d(0, 0, 0)"
-        : "translate3d(0, -1rem, 0)"};
+        : "translate3d(0, -0.95rem, 0)"};
   }
   &::after {
     transform: ${props =>
       props.visible
         ? "rotate(135deg) translate3d(0, 0, 0)"
-        : "translate3d(0, 1rem, 0)"};
+        : "translate3d(0, 0.95rem, 0)"};
   }
 `
