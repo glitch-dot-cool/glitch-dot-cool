734a44e576433dbe62fa95a831ce47a3191086d9
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-10-16 23:38:53 -0400

    WIP UserCard design-system component

---
+++ frontend/src/design-system/UserCard/UserCard.js
@@ -0,0 +1,69 @@
+import React from "react"
+import PropTypes from "prop-types"
+import styled, { css } from "styled-components"
+import Image from "gatsby-image"
+
+const UserCard = ({ user: { author_name: name, avatar } }) => {
+  const imgData = avatar[0]?.formats?.thumbnail?.childImageSharp?.fluid
+
+  return (
+    <Card>
+      {imgData ? <Avatar fluid={imgData} /> : <PlaceholderAvatar />}
+      <Name>{name}</Name>
+    </Card>
+  )
+}
+
+UserCard.propTypes = {
+  user: PropTypes.object,
+}
+
+export default UserCard
+
+const Card = styled.div`
+  display: flex;
+  align-items: center;
+  width: calc(50% - 1rem);
+  background-color: ${props => props.theme.colors.scale_4};
+  padding: 2rem;
+  margin-top: 2rem;
+
+  &:hover {
+    background-color: ${props => props.theme.colors.scale_2};
+
+    h3 {
+      color: ${props => props.theme.colors.scale_5};
+    }
+  }
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    &:last-child {
+      margin-bottom: 2rem;
+    }
+  }
+`
+
+const avatarStyles = css`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+  margin-right: 1rem;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
+  transition: 0.2s ease-out opacity;
+
+  &:hover {
+    opacity: 0.5;
+  }
+`
+
+const Avatar = styled(Image)`
+  ${avatarStyles}
+`
+
+const PlaceholderAvatar = styled.div`
+  ${avatarStyles}
+  background-color: ${props => props.theme.colors.scale_3};
+`
+
+const Name = styled.h3``
