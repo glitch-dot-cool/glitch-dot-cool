a48c3bbb9512817fdbce181036d08d759881f44d
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-10-17 16:56:15 -0400

    add Title component to design-system

---
+++ frontend/src/design-system/Title/Title.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+
+const Title = ({ children }) => {
+  return <StyledTitle>{children}</StyledTitle>
+}
+
+Title.propTypes = {}
+
+export default Title
+
+const StyledTitle = styled.h1`
+  margin-top: 4rem;
+`
