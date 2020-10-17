5b42f45fefaa8c7e40731da349de897aea91a336
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-10-16 23:39:07 -0400

    WIP members page

---
+++ frontend/src/pages/members.js
@@ -0,0 +1,51 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { graphql, useStaticQuery } from "gatsby"
+import styled from "styled-components"
+
+import Layout from "../components/Layout/layout"
+import { UserCard } from "../design-system"
+
+const Members = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      allStrapiAuthor {
+        nodes {
+          author_name
+          avatar {
+            formats {
+              thumbnail {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const members = data.allStrapiAuthor.nodes
+  return (
+    <Layout>
+      <CardContainer>
+        {members.map(member => (
+          <UserCard user={member}>{member.author_name}</UserCard>
+        ))}
+      </CardContainer>
+    </Layout>
+  )
+}
+
+Members.propTypes = {}
+
+export default Members
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`
