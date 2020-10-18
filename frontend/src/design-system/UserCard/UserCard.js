0f5696666f210c48c397768f2391d91f07680a73
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-10-18 14:29:59 -0400

    add styles for small variant of UserCard
    for bylines

--- frontend/src/design-system/UserCard/UserCard.js
+++ frontend/src/design-system/UserCard/UserCard.js
@@ -6,12 +6,16 @@ import Image from "gatsby-image"
 import { Link } from "../../design-system"
 import { slugify } from "../../utils"
 
-const UserCard = ({ user: { author_name: name, avatar } }) => {
+const UserCard = ({ user: { author_name: name, avatar }, size = "large" }) => {
   const imgData = avatar[0]?.formats?.thumbnail?.childImageSharp?.fluid
 
   return (
-    <Card to={`/${slugify(name)}/posts`}>
-      {imgData ? <Avatar fluid={imgData} /> : <PlaceholderAvatar />}
+    <Card size={size} to={`/${slugify(name)}/posts`}>
+      {imgData ? (
+        <Avatar size={size} fluid={imgData} />
+      ) : (
+        <PlaceholderAvatar size={size} />
+      )}
       <Name>{name}</Name>
     </Card>
   )
@@ -26,10 +30,15 @@ export default UserCard
 const Card = styled(Link)`
   display: flex;
   align-items: center;
-  width: calc(50% - 1rem);
+  width: ${props => (props.size === "small" ? `auto` : `calc(50% - 1rem)`)};
   background-color: ${props => props.theme.colors.scale_6};
-  padding: 2rem;
-  margin-top: 2rem;
+  padding: ${props => (props.size === "small" ? `1rem 1.5rem` : `2rem`)};
+  margin-top: ${props => (props.size === "small" ? `0` : `2rem`)};
+  border-radius: ${props => (props.size === "small" ? `3px` : `0px`)};
+
+  h2 {
+    font-size: ${props => (props.size === "small" ? `1.4rem` : `auto`)};
+  }
 
   &:hover {
     background-color: ${props => props.theme.colors.scale_2};
@@ -40,7 +49,7 @@ const Card = styled(Link)`
   }
 
   @media (max-width: 1000px) {
-    width: 100%;
+    width: ${props => (props.size === "small" ? `inherit` : `100%`)};
     &:last-child {
       margin-bottom: 2rem;
     }
@@ -48,12 +57,14 @@ const Card = styled(Link)`
 `
 
 const avatarStyles = css`
-  width: 4rem;
-  height: 4rem;
+  width: ${props => (props.size === "small" ? `2.4rem` : `4rem`)};
+  height: ${props => (props.size === "small" ? `2.4rem` : `4rem`)};
   border-radius: 50%;
   margin-right: 1rem;
-  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 0px 5px ${props => props.theme.colors.box_shadow};
   transition: 0.2s ease-out opacity;
+  /* border: 1px solid ${props => props.theme.colors.scale_2}; */
+  ${props => console.log(props.theme.colors.box_shadow)}
 
   &:hover {
     opacity: 0.5;
