8d4e4cfa32babf6d28e00a2d649fcbd439522a46
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-10-18 15:19:40 -0400

    use Avatar DS component in UserCard

--- frontend/src/design-system/UserCard/UserCard.js
+++ frontend/src/design-system/UserCard/UserCard.js
@@ -1,9 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
-import styled, { css } from "styled-components"
-import Image from "gatsby-image"
+import styled from "styled-components"
 
-import { Link } from "../../design-system"
+import { Link, Avatar } from "../../design-system"
 import { slugify } from "../../utils"
 
 const UserCard = ({ user: { author_name: name, avatar }, size = "large" }) => {
@@ -11,11 +10,7 @@ const UserCard = ({ user: { author_name: name, avatar }, size = "large" }) => {
 
   return (
     <Card size={size} to={`/${slugify(name)}/posts`}>
-      {imgData ? (
-        <Avatar size={size} fluid={imgData} />
-      ) : (
-        <PlaceholderAvatar size={size} />
-      )}
+      <Avatar image={imgData} />
       <Name>{name}</Name>
     </Card>
   )
@@ -54,30 +49,6 @@ const Card = styled(Link)`
       margin-bottom: 2rem;
     }
   }
-`
-
-const avatarStyles = css`
-  width: ${props => (props.size === "small" ? `2.4rem` : `4rem`)};
-  height: ${props => (props.size === "small" ? `2.4rem` : `4rem`)};
-  border-radius: 50%;
-  margin-right: 1rem;
-  box-shadow: 0px 0px 5px ${props => props.theme.colors.box_shadow};
-  transition: 0.2s ease-out opacity;
-  /* border: 1px solid ${props => props.theme.colors.scale_2}; */
-  ${props => console.log(props.theme.colors.box_shadow)}
-
-  &:hover {
-    opacity: 0.5;
-  }
-`
-
-const Avatar = styled(Image)`
-  ${avatarStyles}
-`
-
-const PlaceholderAvatar = styled.div`
-  ${avatarStyles}
-  background-color: ${props => props.theme.colors.scale_3};
 `
 
 const Name = styled.h2``
