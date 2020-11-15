105ec2663b1ba85137fab3df4ab2a6846db8f1c5
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-11-14 20:09:24 -0500

    add tags, author ids to blog post query

--- frontend/src/queries/blogPosts.js
+++ frontend/src/queries/blogPosts.js
@@ -1,40 +1,45 @@
 module.exports = `
-query {
-  allStrapiPost {
-    nodes {
-      type
-      title
-      skip_frontpage
-      published_date(formatString: "MMMM DD, YYYY")
-      links {
-        url
+  query {
+    allStrapiPost {
+      nodes {
+        type
         title
-        id
-      }
-      body
-      slug
-      authors {
-        author_name
-        avatar {
+        skip_frontpage
+        published_date(formatString: "MMMM DD, YYYY")
+        links {
+          url
+          title
           id
-          formats {
-            thumbnail {
-              image {
-                childImageSharp {
-                  fluid {
-                    src
-                    srcSet
-                    base64
-                    aspectRatio
-                    sizes
+        }
+        body
+        slug
+        authors {
+          id
+          author_name
+          avatar {
+            id
+            formats {
+              thumbnail {
+                image {
+                  childImageSharp {
+                    fluid {
+                      src
+                      srcSet
+                      base64
+                      aspectRatio
+                      sizes
+                    }
                   }
                 }
               }
             }
           }
         }
+        tags {
+          id
+          tag
+        }
       }
     }
   }
-}
 `
