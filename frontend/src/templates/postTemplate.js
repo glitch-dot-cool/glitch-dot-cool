0511f151af68933cbc0b1725ec92cbc394ffaabf
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-11-14 22:54:06 -0500

    render link buttons in posts

--- frontend/src/templates/postTemplate.js
+++ frontend/src/templates/postTemplate.js
@@ -5,7 +5,7 @@ import styled from "styled-components"
 import PostHeader from "../components/Posts/PostHeader/PostHeader"
 import Layout from "../components/Layout/layout"
 import MarkdownHTML from "../components/Transforms/MarkdownHTML"
-import { Tag } from "../design-system"
+import { Tag, Button } from "../design-system"
 import { lightTheme as theme } from "../design-system/theme"
 
 const { baseMonoMixin } = theme.text
@@ -18,13 +18,26 @@ const postLayout = ({ pageContext: post }) => {
         title={post.title}
         publishDate={post.published_date}
       />
+
       <MarkdownHTML source={post.body} />
-      <TagHeader>tags:</TagHeader>
-      <Tags>
-        {post.tags.map(({ tag, id }) => (
-          <Tag key={id}>{tag}</Tag>
-        ))}
-      </Tags>
+
+      <Links>
+        <LinkHeader>links:</LinkHeader>
+        <LinkGroup>
+          {post.links.map(link => (
+            <Button key={link.id} href={link.url}>
+              {link.title}
+            </Button>
+          ))}
+        </LinkGroup>
+
+        <LinkHeader>tags:</LinkHeader>
+        <LinkGroup>
+          {post.tags.map(({ tag, id }) => (
+            <Tag key={id}>{tag}</Tag>
+          ))}
+        </LinkGroup>
+      </Links>
     </Layout>
   )
 }
@@ -35,7 +48,11 @@ postLayout.propTypes = {
 
 export default postLayout
 
-const Tags = styled.div`
+const Links = styled.div`
+  margin-top: 2rem;
+`
+
+const LinkGroup = styled.div`
   display: flex;
   margin: 1rem 0 2rem 0;
 
@@ -44,7 +61,7 @@ const Tags = styled.div`
   }
 `
 
-const TagHeader = styled.p`
+const LinkHeader = styled.p`
   ${baseMonoMixin}
   font-size: 1.6rem;
 `
