78b9793ddd97c5bd06a0ac6717f362df0b5c7853
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-11-14 20:05:54 -0500

    fix img overflow in posts

--- frontend/src/components/Transforms/Image.js
+++ frontend/src/components/Transforms/Image.js
@@ -1,9 +1,14 @@
 import React from "react"
+import styled from "styled-components"
 
 const Image = ({ src, alt }) => {
   // @TODO implement gatsby-image
   // there's an open PR about this: https://github.com/strapi/gatsby-source-strapi/pull/122
-  return <img src={src} alt={alt} />
+  return <Img src={src} alt={alt} />
 }
 
 export default Image
+
+const Img = styled.img`
+  max-width: 100%;
+`
