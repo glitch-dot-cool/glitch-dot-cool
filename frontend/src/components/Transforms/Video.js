f818e5441909d88b3455c2600df716fde35fe0a5
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-11-14 20:34:59 -0500

    make Video component responsive

--- frontend/src/components/Transforms/Video.js
+++ frontend/src/components/Transforms/Video.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Video = ({ src }) => {
   return (
-    <video width="1024" height="720" controls>
+    <video width="100%" height="auto" controls>
       <source src={src} type="video/mp4" />
     </video>
   )
