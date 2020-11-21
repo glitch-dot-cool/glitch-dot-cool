4a8ebf809dc687d384aef952ee5f93113884af4b
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-11-21 13:28:10 -0500

    add post query to /home route

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,43 +1,64 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/Layout/layout"
 import SEO from "../components/Layout/seo"
 
-import MarkdownHTML from "../components/Transforms/MarkdownHTML"
-import { Tag, Button } from "../design-system"
-
-const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allStrapiPost {
-        edges {
-          node {
-            body
-          }
-        }
-      }
-    }
-  `)
-
-  const post = data.allStrapiPost.edges[0].node.body
-
+const IndexPage = ({
+  data: {
+    allStrapiPost: { nodes: posts },
+  },
+}) => {
+  console.log(posts)
   return (
     <Layout>
       <SEO title="Home" />
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <MarkdownHTML source={post} />
-      </div>
-      <Tag>something</Tag>
-      <Tag>whatever</Tag>
-      <Tag>thing</Tag>
-      <Tag>genre</Tag>
-
-      <Button href="https://google.com">hello</Button>
-      <Button to="/members">world</Button>
-      <Button onClick={() => alert("clicked!")}>test</Button>
     </Layout>
   )
 }
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allStrapiPost(filter: { skip_frontpage: { eq: false } }) {
+      nodes {
+        slug
+        strapiId
+        published_date(formatString: "MMMM DD, YYYY")
+        authors {
+          author_name
+          avatar {
+            formats {
+              thumbnail {
+                image {
+                  childImageSharp {
+                    fluid {
+                      src
+                      srcSet
+                      base64
+                      aspectRatio
+                      sizes
+                    }
+                    id
+                  }
+                }
+              }
+            }
+          }
+        }
+        thumbnail {
+          childImageSharp {
+            fluid {
+              src
+              srcSet
+              base64
+              aspectRatio
+              sizes
+            }
+          }
+        }
+      }
+    }
+  }
+`
