e1d39873278300484e9fc083660dc7f114c7d6a8
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-11-28 17:08:29 -0500

    fix micro avatar hover interaction

--- frontend/src/design-system/UserCard/UserCard.js
+++ frontend/src/design-system/UserCard/UserCard.js
@@ -26,10 +26,10 @@ const UserCard = ({
         onMouseOver={() => setIsTooltipVisible(true)}
         onMouseOut={() => setIsTooltipVisible(false)}
       >
-        <MicroAvatar image={imgData} size="small" index={index} />
         <Tooltip isVisible={isTooltipVisible} index={index}>
           {name}
         </Tooltip>
+        <MicroAvatar image={imgData} size="small" index={index} />
       </MicroCard>
     )
   }
@@ -93,6 +93,24 @@ const Name = styled.h2``
 
 const MicroCard = styled(Link)``
 
+const Tooltip = styled.span`
+  display: ${({ isVisible }) => (isVisible ? "inline" : "none")};
+  position: absolute;
+  margin-top: 8px;
+  margin-left: ${({ index }) => (index === 0 ? "0" : "-20px")};
+  background-color: black;
+  color: white;
+  font-family: "Roboto-Mono", monospace;
+  font-weight: 100;
+  font-size: 1.3rem;
+  z-index: 2;
+
+  :hover + div {
+    transform: scale(1.2);
+    opacity: 0.9;
+  }
+`
+
 const MicroAvatar = styled(Avatar)`
   width: 3rem;
   height: 3rem;
@@ -105,17 +123,4 @@ const MicroAvatar = styled(Avatar)`
     transform: scale(1.2);
     opacity: 0.9;
   }
-`
-
-const Tooltip = styled.span`
-  display: ${({ isVisible }) => (isVisible ? "inline" : "none")};
-  position: absolute;
-  margin-top: -22px;
-  margin-left: ${({ index }) => (index === 0 ? "0" : "-20px")};
-  background-color: black;
-  color: white;
-  font-family: "Roboto-Mono", monospace;
-  font-weight: 100;
-  font-size: 1.3rem;
-  z-index: 2;
 `
