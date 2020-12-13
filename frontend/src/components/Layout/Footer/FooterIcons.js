29b883f2490e51385cabf32dca53f25fd1c34144
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-12-13 16:59:05 -0500

    fix footer link hover state

--- frontend/src/components/Layout/Footer/FooterIcons.js
+++ frontend/src/components/Layout/Footer/FooterIcons.js
@@ -50,12 +50,6 @@ const IconsContainer = styled.div`
 
   a {
     text-decoration: none;
-    color: ${props => props.theme.colors.scale_1};
-  }
-
-  a:hover svg {
-    color: ${props => props.theme.colors.social_icon_hover};
-    animation: ${flicker} 0.2s forwards;
   }
 
   a:not(:last-of-type) {
@@ -65,12 +59,20 @@ const IconsContainer = styled.div`
 
 const Icon = styled(FontAwesomeIcon)`
   font-size: 2rem;
+
+  :hover {
+    animation: ${flicker} 0.2s forwards;
+  }
 `
 
 const FooterLinks = styled(LinkIcons)`
   svg {
     margin-bottom: 1rem;
     color: ${props => props.theme.colors.scale_3};
+  }
+
+  a {
+    background: transparent !important;
   }
 
   @media only screen and (max-width: ${theme.measurements
