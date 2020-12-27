5d2670a3544b83fa3230418fce0c67382eaa5c9e
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-12-27 16:24:28 -0500

    fix deploy target dir

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 
-rsync -r --delete-after --quiet -e "ssh -i ~/.ssh/strapi-deploy" $TRAVIS_BUILD_DIR oddlogic@45.33.76.67:~/glitch-dot-cool
+rsync -r --delete-after --quiet -e "ssh -i ~/.ssh/strapi-deploy" $TRAVIS_BUILD_DIR oddlogic@45.33.76.67
