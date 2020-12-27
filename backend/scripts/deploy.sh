db0692c0e9e807659109aaf181987803172c8123
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-12-27 14:04:12 -0500

    provide identity file to deploy script

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/glitch-dot-cool oddlogic@45.33.76.67:~/glitch-dot-cool
+rsync -r --delete-after --quiet -e "ssh -i $HOME/.ssh/strapi-deploy" $TRAVIS_BUILD_DIR/glitch-dot-cool oddlogic@45.33.76.67:$HOME/glitch-dot-cool
