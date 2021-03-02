337944fb98b2aeff9300eb2a84ea9e69446a38e0
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2021-03-01 19:12:04 -0500

    shrink and center images in posts

--- frontend/src/components/Transforms/Image.js
+++ frontend/src/components/Transforms/Image.js
@@ -4,11 +4,21 @@ import styled from "styled-components"
 const Image = ({ src, alt }) => {
   // @TODO implement gatsby-image
   // there's an open PR about this: https://github.com/strapi/gatsby-source-strapi/pull/122
-  return <Img src={src} alt={alt} />
+  return (
+    <Wrapper>
+      <Img src={src} alt={alt} />
+    </Wrapper>
+  )
 }
 
 export default Image
 
 const Img = styled.img`
-  max-width: 100%;
+  max-width: 50%;
+  margin: 3rem 0;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
 `
