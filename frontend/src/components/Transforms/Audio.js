3e4b8b75b2a65772e1440b01586e8e67a88a5575
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2021-04-05 18:25:26 -0400

    set audio element controlsList attribute to nodownload

--- frontend/src/components/Transforms/Audio.js
+++ frontend/src/components/Transforms/Audio.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Audio = ({ src }) => {
   return (
-    <audio controls>
+    <audio controls controlsList="nodownload">
       <source src={src} type="audio/mp3" />
     </audio>
   )
